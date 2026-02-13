4069ee70379df6497e9abcda4c6ba24ead1e708d
João Igor Paulino Pinto <joaoigorpaulinopinto@gmail.com>
2026-02-13 16:25:48 -0300

    fix exceptions

--- src/app/controllers/ProjectController.cs
+++ src/app/controllers/ProjectController.cs
@@ -7,7 +7,7 @@ using static envmanager.src.data.service.dtos.ProjectDtos;
 namespace envmanager.src.app.controllers
 {
     [ApiController]
-    [Route("project")] // Rota explícita em minúsculo é boa prática
+    [Route("project")]
     public class ProjectController : ControllerBase
     {
         private readonly ICreateProjectUseCase _createProjectUseCase;
@@ -21,64 +21,35 @@ namespace envmanager.src.app.controllers
             _updateProjectVariables = updateProjectVariables;
         }
 
+        private string userId => User.Claims.FirstOrDefault(c => c.Type == "id")?.Value
+                           ?? throw new UnauthorizedAccessException("Usuário inválido no token.");
 
+        [HttpPost]
         [Authorize]
         [HttpPost]
         public async Task<ActionResult> Create([FromBody] CreateProjectRequest createProjectRequest)
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
-            if (string.IsNullOrEmpty(userId))
-            {
-                throw new UnauthorizedAccessException();
-            }
             var created = await _createProjectUseCase.Execute(createProjectRequest, userId);
-
-            if (!created)
-            {
-                throw new Exception();
-            }
-
             return Created("", new { message = "Project successfully created" });
         }
         [Authorize]
         [HttpGet]
         public async Task<List<GetProjectsResponse>> Get()
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
-            if (string.IsNullOrEmpty(userId))
-            {
-                throw new UnauthorizedAccessException();
-            }
             return await _getProjectsUseCase.Execute(userId);
         }
         [Authorize]
         [HttpGet("{id}")]
         public async Task<GetProjectByIdResponse> Get([FromRoute]string id)
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
-            if (string.IsNullOrEmpty(userId))
-            {
-                throw new UnauthorizedAccessException();
-            }
             return await _getProjectsUseCase.Execute(userId, id);
         }
         [Authorize]
         [HttpPut]
         public async Task<ActionResult> UpdateVariables([FromBody] UpdateVariablesRequest updateVariablesRequest)
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "id")?.Value; 
-            if (string.IsNullOrEmpty(userId))
-            {
-                throw new UnauthorizedAccessException();
-            }
             var updated = await _updateProjectVariables.Execute(updateVariablesRequest, userId);
-
-            if(updated) return Ok(new { message = "Project successfully created" });
-            else
-            {
-                throw new Exception();
-            }
-             
+            return Ok(new { message = "Project successfully created" });           
         }
     }
 }
